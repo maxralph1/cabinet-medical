5d2ddc8882b657257c91860f6a813fbdb0b261d1
Max Ralph <maxralph01@gmail.com>
2025-04-06 15:08:23 +0400

    changed images on the service section of homepage

--- client/src/utils/SwalAlert.jsx
+++ client/src/utils/SwalAlert.jsx
@@ -3,8 +3,9 @@ import swal from 'sweetalert2';
 
 const SwalAlert = (type, status, message) => {
     swal.fire({
-        text: `${status && status}${(status && message) && ': '}${message && message}`,
-        color: `${(type=='error') ? '#900000' : '#f2f2f2' }`,
+        // text: `${status && status}${(status && message) && ': '}${message && message}`,
+        text: `${message && message}`,
+        color: `${(type=='error') ? '#900000' : '#f2f2f25' }`,
         width: 325,
         position: 'top',
         showConfirmButton: false
